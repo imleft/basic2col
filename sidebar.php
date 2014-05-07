4dfaf1f815ef7b997d95e6f22fc7a6cf411e31d3
Nick Gasson <nick@nickg.me.uk>
2014-05-07 08:39:49 +0100

    Fix validation error

--- sidebar.php
+++ sidebar.php
@@ -8,7 +8,7 @@
 
 	<li id="search">
 	<form id="searchform" method="get" action="<?php bloginfo('url') ?>">
-	
+
 		<h2><label for="s"><?php _e('Search','basic2col'); ?></label></h2>
 		<div>
 			<input type="text" value="" name="s" id="s" />
@@ -24,10 +24,10 @@
 	<?php if ( function_exists('dynamic_sidebar') && dynamic_sidebar('Sidebar 1') ) : else : ?>
 		<!-- Things /not/ included if dynamic sidebar activated -->
 	<?php endif; ?>
-	</ul>
 
 	<?php wp_list_categories('sort_column=name&show_count=1&title_li=<h2>' . __('Categories','basic2col') . '</h2>'); ?>
-	
+	</ul>
+
 	<ul>
 		<li id="randomlinks"><h2><?php _e('Random links','basic2col'); ?></h2>
 			<ul>
@@ -49,7 +49,7 @@
 		<li><h2><?php _e('Meta','basic2col'); ?></h2>
 		<ul>
 			<li>	<a href="<?php bloginfo('rss2_url'); ?>">
-				<img src="<?php bloginfo('template_url')?>/gfx/rss.png" alt="RSS" class="rssimg" /> 
+				<img src="<?php bloginfo('template_url')?>/gfx/rss.png" alt="RSS" class="rssimg" />
 				<?php _e('Posts','basic2col'); ?></a></li>
 			<li>	<a href="<?php bloginfo('comments_rss2_url'); ?>">
 				<img src="<?php bloginfo('template_url')?>/gfx/rss.png" alt="RSS" class="rssimg" />
@@ -61,7 +61,7 @@
 			<?php /*add some support for MU*/ if(is_basic2col_wpmu()) { global $current_site; ?>
 			<li><a href="http://<?php echo $current_site->domain . $current_site->path ?>" title="<?php _e('Hosted by','basic2col'); ?> <?php echo $current_site->site_name ?>">
 			<?php echo $current_site->site_name ?></a></li>
-		
+
 			<?php } wp_meta(); ?>
 		</ul>
 		</li>
