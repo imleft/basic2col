b4b164e1d983352f6b0a13664670337ff3460427
Nick Gasson <nick@nickg.me.uk>
2015-02-01 21:02:21 +0000

    Inline google analytics cookie

--- header.php
+++ header.php
@@ -1,9 +1,9 @@
-<?php 
+<?php
 /*language stuff*/
-if(function_exists('load_betterlang_textdomain')) { load_betterlang_textdomain('basic2col'); } 
+if(function_exists('load_betterlang_textdomain')) { load_betterlang_textdomain('basic2col'); }
 	else { basic2col_textdomain(); }
 /*doctype*/
-	echo basic2col_doctype(); 
+	echo basic2col_doctype();
 ?>
 
 <!-- Don't underestimate the Power of the Source -->
@@ -26,6 +26,15 @@ if(function_exists('load_betterlang_textdomain')) { load_betterlang_textdomain('
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 	<?php wp_get_archives('type=monthly&format=link'); ?>
 	<?php wp_head(); ?>
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-8194326-1', 'auto');
+          ga('send', 'pageview');
+        </script>
 </head>
 
 <body id="top" class="<?php do_action('basic2col_bodyclass'); ?>">
