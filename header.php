9122d964aab52da04e1e36e24cab34948d80ea72
Nick Gasson <nick@nickg.me.uk>
2017-05-13 12:43:07 +0000

    Remove Google analytics

--- header.php
+++ header.php
@@ -25,16 +25,6 @@
   <?php /* wp_get_archives('type=monthly&format=link'); */ ?>
   <?php /* if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); */ ?>
   <?php wp_head(); ?>
-  <?php if (!is_user_logged_in()) { ?>
-    <script type="text/javascript">
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-      ga('create', 'UA-8194326-1', 'auto');
-      ga('send', 'pageview');
-    </script>
-  <?php } ?>
 </head>
 
 <body id="top" class="<?php do_action('basic2col_bodyclass'); ?>">
