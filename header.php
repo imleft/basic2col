da5a73815bcb62341e975489429f59ff706e2dbc
Nick Gasson <nick@nickg.me.uk>
2020-02-04 05:03:11 +0000

    Optimise for mobile

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
 <head>
   <meta http-equiv="Content-Type"
         content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>
     <?php wp_title(' | ',true,'right'); ?> <?php bloginfo('name'); ?>
     <?php if (is_home() || is_front_page()) { ?> : <?php bloginfo('description'); ?><?php } ?>
